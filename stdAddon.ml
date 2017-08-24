6fd4b6d234b99920d2cb1a82b6da2d25338575d0
Audouin Maxime <coucou747@gmail.com>
2017-08-24 21:34:58 +0200

    moving (--) to stdAddon.ml

--- stdAddon.ml
+++ stdAddon.ml
@@ -3,3 +3,9 @@ let fold1 f li =
   | hd::tl -> List.fold_left f hd tl
   | _ -> assert false
     
+
+let rec seq_append a b li =
+  if a > b then li
+  else seq_append a (b - 1) (b :: li)
+
+let (--) a b = seq_append a b []
--- tictactoe.ml
+++ tictactoe.ml
@@ -1,11 +1,6 @@
 open Ai
-
-let rec seq_append a b li =
-  if a > b then li
-  else seq_append a (b - 1) (b :: li)
-
-let (--) a b = seq_append a b []
-    
+open StdAddon
+  
 module TicState : Game = struct
   
   type player = Remplissage.player
